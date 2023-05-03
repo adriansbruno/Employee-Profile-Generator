a6c5282ee703c374e56ce46ce1576afc7e88681e
adriansbruno <114193634+adriansbruno@users.noreply.github.com>
2023-05-02 22:18:17 -0400

    Update Employee.test.js

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,1 +1,4 @@
-const Employee = require("../lib/employee")
+const Employee = require("../lib/Employee")
+
+const Employee = require('../lib/Employee');                              // import
+const employee = new Employee ('Adrian', '8493847', 'test@gmail.com'); 
