3055124623ad182f80b745e879a88e85ea19f9a4
adriansbruno <114193634+adriansbruno@users.noreply.github.com>
2023-05-02 22:24:12 -0400

    Update Engineer.test.js

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,1 +1,8 @@
 const Engineer = require('../lib/Engineer');
+
+
+test('new Engineer will return user input name', () => {
+    const test = new Engineer('Henry', 22, 'henry@gmail.com', 'Henry')
+    
+    expect(test.getName()).toBe('Henry')
+})
